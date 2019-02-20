5427ac52256ae46922bdc82e7edf26c267239483
Tom Stejskal <tom.stejskal@gmail.com>
2019-02-20 12:37:43 +0100

    Do not allow to overwrite Assets.js and Assets.elm

--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,7 @@ import Data.Semigroup ((<>))
 import qualified Data.Text.IO as Text
 import Options.Applicative ((<**>))
 import qualified Options.Applicative as Opt
+import qualified System.Directory as Dir
 
 import qualified Asset
 import qualified JS
@@ -16,13 +17,23 @@ import qualified Options
 
 main :: IO ()
 main = do
-  Options{..} <- Opt.execParser opts
-  asset <- Asset.fromPath path
-  Text.writeFile "Assets.elm" $ Elm.gen asset
-  Text.writeFile "Assets.js" $ JS.gen asset
+  Options{..} <- Opt.execParser optInfo
+  anyFileExists <- (||) <$> Dir.doesFileExist elmFile <*> Dir.doesFileExist jsFile
+  if anyFileExists && not forceOverwrite then
+    let
+      errorMsg = jsFile <> " and/or " <> elmFile <> " exists, use -f flag to overwrite these files"
+    in
+    Opt.handleParseResult . Opt.Failure $
+      Opt.parserFailure Opt.defaultPrefs optInfo (Opt.ErrorMsg errorMsg) []
+  else do
+    asset <- Asset.fromPath path
+    Text.writeFile elmFile $ Elm.gen asset
+    Text.writeFile jsFile $ JS.gen asset
   where
-    opts = Opt.info (Options.parser <**> Opt.helper)
+    optInfo = Opt.info (Options.parser <**> Opt.helper)
       ( Opt.fullDesc
       <> Opt.progDesc "Generates Elm assets"
       <> Opt.header "elm-asset-gen - Elm assets generator"
       )
+    jsFile = "Assets.js"
+    elmFile = "Assets.elm"
--- src/Options.hs
+++ src/Options.hs
@@ -5,15 +5,23 @@ module Options
   , parser
   ) where
 
+import Data.Semigroup ((<>))
 import qualified Options.Applicative as Opt
 import System.FilePath (FilePath)
 
 data Options = Options
   { path :: FilePath
+  , forceOverwrite :: Bool
   }
 
 parser :: Opt.Parser Options
 parser =
-  Options <$> pathArg
+  Options <$> pathArg <*> forceOverwriteSwitch
   where
-    pathArg = Opt.argument Opt.str (Opt.metavar "PATH")
+    pathArg =
+      Opt.argument Opt.str (Opt.metavar "PATH")
+    forceOverwriteSwitch =
+      Opt.switch $
+        Opt.long "force-overwrite"
+        <> Opt.short 'f'
+        <> Opt.help "Force overwrite output files"
