0f146411b37d27c9f7b3a10b9a60498babe4f76c
JOME <josef.melicharek@abra.eu>
2019-02-28 09:06:32 +0100

    Update golden tests

--- test/Assets.elm
+++ test/Assets.elm
@@ -4,20 +4,20 @@ import Json.Decode as Decode
 import Json.Decode.Extra as Decode
 
 
-type alias Icons =
-    { arrowUpSvg : String
-    , arrowDownSvg : String
-    }
-
-
 type alias Fonts =
     { gothamOtf : String
     }
 
 
+type alias Icons =
+    { arrowDownSvg : String
+    , arrowUpSvg : String
+    }
+
+
 type alias Assets =
-    { icons : Icons
-    , fonts : Fonts
+    { fonts : Fonts
+    , icons : Icons
     }
 
 
@@ -25,15 +25,15 @@ decoder : Decode.Decoder Assets
 decoder =
     Decode.succeed Assets
         |> Decode.andMap
-            (Decode.field "icons"
-                (Decode.succeed Icons
-                    |> Decode.andMap (Decode.field "arrowUpSvg" Decode.string)
-                    |> Decode.andMap (Decode.field "arrowDownSvg" Decode.string)
-                )
-            )
-        |> Decode.andMap
             (Decode.field "fonts"
                 (Decode.succeed Fonts
                     |> Decode.andMap (Decode.field "gothamOtf" Decode.string)
+                )
+            )
+        |> Decode.andMap
+            (Decode.field "icons"
+                (Decode.succeed Icons
+                    |> Decode.andMap (Decode.field "arrowDownSvg" Decode.string)
+                    |> Decode.andMap (Decode.field "arrowUpSvg" Decode.string)
                 )
             )
--- test/Assets.js
+++ test/Assets.js
@@ -1,13 +1,13 @@
-import assets_icons_arrowUpSvg from "test/assets/icons/arrow-up.svg";
-import assets_icons_arrowDownSvg from "test/assets/icons/arrow-down.svg";
 import assets_fonts_gothamOtf from "test/assets/fonts/Gotham.otf";
+import assets_icons_arrowDownSvg from "test/assets/icons/arrow-down.svg";
+import assets_icons_arrowUpSvg from "test/assets/icons/arrow-up.svg";
 
 export default {
-  icons: {
-    arrowUpSvg: assets_icons_arrowUpSvg,
-    arrowDownSvg: assets_icons_arrowDownSvg
-  },
   fonts: {
     gothamOtf: assets_fonts_gothamOtf
+  },
+  icons: {
+    arrowDownSvg: assets_icons_arrowDownSvg,
+    arrowUpSvg: assets_icons_arrowUpSvg
   }
 };
